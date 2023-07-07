6463b724bf840618351977440c84126b4b6603c1
emagweb <emagcodeshake@gmail.com>
2023-07-07 14:01:38 +0300

    404

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,8 +1,34 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Helmet } from 'react-helmet'
+
+import land from '../data/land'
+import man from '../assets/svg/home/man.svg'
 
 function NotFound() {
   return (
-    <div>404 Error</div>
+    <>
+      <div className="s-100"></div>
+      <div className="row">
+        <div className="col-lg-2"></div>
+        <div className="col-lg-5">
+          <Helmet>
+            <h2>{land[0].ru.err.subtitle}</h2>
+          </Helmet>
+          <h2 className="title-part c-dark">{land[0].ru.err.subtitle}</h2>
+          <div className="s-10"></div>
+          <h3 className="title-intro mini c-blue">{land[0].ru.err.title}</h3>
+          <div className="s-10"></div>
+          <p>{land[0].ru.err.text}</p>
+          <div className="s-10"></div>
+          <Link to='/' className='button'>{land[0].ru.err.link}</Link>
+        </div>
+        <div className="col-lg-5">
+          <img src={man} alt={land[0].ru.err.subtitle} />
+        </div>
+      </div>
+      <div className="s-100"></div>
+    </>
   )
 }
 
---
+++ src/pages/Privacy.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import {Hero} from '../components/Hero'
+import {About} from '../components/About'
+import {Reviews} from '../components/Reviews'
+import {Steps} from '../components/Steps'
+import {Features} from '../components/Features'
+import {Bot} from '../components/Bot'
+import {Vacancies} from '../components/Vacancies'
+import {Form} from '../components/Form'
+
+function Privacy() {
+  return (
+    <>
+      <Hero />
+      <About  />
+      <Reviews />
+      <Steps  />
+      <Features />
+      <Bot />
+      <Vacancies  />
+      <Form />
+    </>
+  )
+}
+
+export {Privacy}
