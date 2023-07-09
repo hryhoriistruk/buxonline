7c6f9584f6d255f037b0d770eb90e6bee1e49b10
emagweb <emagcodeshake@gmail.com>
2023-07-09 10:43:25 +0300

    support banner

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React from "react"
-import { Routes, Route } from "react-router-dom"
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 import Container from 'react-bootstrap/Container'
-import "./styles/custom.sass"
+import './styles/custom.sass'
 
-import { Footer } from "./components/Footer"
-import { Nav } from "./components/Nav"
+import { Footer } from './components/Footer'
+import { Nav } from './components/Nav'
 
-import { Home } from "./pages/Home"
-import { Privacy } from "./pages/Privacy"
-import { Cookie } from "./pages/Cookie"
-import { NotFound } from "./pages/NotFound"
+import { Home } from './pages/Home'
+import { Privacy } from './pages/Privacy'
+import { Cookie } from './pages/Cookie'
+import { NotFound } from './pages/NotFound'
+
+import support from './assets/svg/home/standwith.svg'
 
 const App = () => {
   return (
     <>
+      <img src={support} alt={'We stand with Ukraine'} className='support' />
       <Container>
         <Nav />
       </Container>
