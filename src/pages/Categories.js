cccc719786a0d00f32ab34808dcfeea4664cadb1
emagweb <emagcodeshake@gmail.com>
2023-07-31 17:16:20 +0300

    fix categories

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -6,16 +6,13 @@ const apiUrl = 'https://api-dev.buxonline.org/api/v1/category/list/'
 
 function Categories() {
   const [categories, setCategories] = useState([])
-  const [currentPage, setCurrentPage] = useState(1)
-  const [totalPages, setTotalPages] = useState(1)
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(`${apiUrl}?page=${currentPage}`)
-        setCategories(response.data.results)
-        setTotalPages(response.data.total_pages)
+        const response = await axios.get(apiUrl)
+        setCategories(response.data) // Изменяем здесь, присваивая response.data напрямую
         setLoading(false)
       } catch (error) {
         console.error('Ошибка при запросе:', error)
@@ -24,19 +21,7 @@ function Categories() {
     }
 
     fetchData()
-  }, [currentPage])
-
-  const handleNextPage = () => {
-    if (currentPage < totalPages) {
-      setCurrentPage(currentPage + 1)
-    }
-  }
-
-  const handlePrevPage = () => {
-    if (currentPage > 1) {
-      setCurrentPage(currentPage - 1)
-    }
-  }
+  }, [])
 
   if (loading) {
     return <div>Loading...</div>
@@ -49,15 +34,12 @@ function Categories() {
       <div className="s-50"></div>
       <div className="w-100 tc">
         {categories.map((el) => (
-          <Link to={`/categories/${el.role}`} className='button cat m-1' key={el.id}>{el.role}</Link>
+          <Link to={`/categories/${el.role}`} className='button cat m-1' key={el.id}>
+            {el.role}
+          </Link>
         ))}
       </div>
       <div className="s-100"></div>
-      <div className='flex w-80 ma pb-5 jcc aic'>
-        <button onClick={handlePrevPage} disabled={currentPage === 1} className='button'>Назад</button>
-        <span className='px-4'>Сторiнка {currentPage} з {totalPages}</span>
-        <button onClick={handleNextPage} disabled={currentPage === totalPages} className='button'>Вперед</button>
-      </div>
       <div className="s-100"></div>
     </>
   )
