c43c7acc06b6800ab1d51e5e59ad7f7753218259
emagweb <emagcodeshake@gmail.com>
2023-08-05 20:45:45 +0300

    add languages to menu

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,16 +3,17 @@ import { Link } from 'react-router-dom'
 import Navbar from 'react-bootstrap/Navbar'
 import { LangMenu } from './elements/LangMenu'
 import land from '../data/land'
-import cats from '../data/cats'
+import lang from '../data/lang'
 import Logo from '../logo.svg'
 
 function Nav() {
-
   const [language, setLanguage] = useState(localStorage.getItem('language') || 'uk')
 
   useEffect(() => {
     localStorage.setItem('language', language)
   }, [language])
+
+  const currentLanguage = lang.find((el) => el.code === language)
 
   return (
     <Navbar className="nav-top py-3">
@@ -22,15 +23,19 @@ function Nav() {
         <span className="pl-2 mt-2 title-logo">Buxonline</span>
       </Link>
       <Navbar.Toggle />
-      <Navbar.Collapse className="justify-content-end">
-        <Link onClick={() => setLanguage(language)} to={`/categories/${language}`} className="mx-4 c-dark upp">
-          {cats[0].ru.page[1]}
-        </Link>
-        <Link to={`/${land[0].ru.nav.link[1]}/${language}`} className="mx-4 c-dark upp">
-          {land[0].ru.nav.link[0]}
-        </Link>
-        <a href={land[0].ru.nav.button[1]} className='button nav-button m-hide' target='blank'>{land[0].ru.nav.button[0]}</a>
-      </Navbar.Collapse>
+      {currentLanguage ? (
+        <Navbar.Collapse className="justify-content-end">
+          <Link onClick={() => setLanguage(language)} to={`/categories/${language}`} className="mx-4 c-dark upp">
+            {currentLanguage.menu[0]}
+          </Link>
+          <Link to={`/${land[0].ru.nav.link[1]}/${language}`} className="mx-4 c-dark upp">
+            {currentLanguage.menu[1]}
+          </Link>
+          <a href={land[0].ru.nav.button[1]} className="button nav-button m-hide" target="blank">
+            {currentLanguage.menu[2]}
+          </a>
+        </Navbar.Collapse>
+      ) : null}
       <div className="mx-3"></div>
       <LangMenu setLanguage={setLanguage} />
     </Navbar>
--- src/data/lang.js
+++ src/data/lang.js
@@ -2,92 +2,111 @@ const lang = [
   {
     code: "uk",
     pic: require('../assets/svg/flags/4x3/ua.svg').default,
-    name: "Ukrainian"
+    name: "Ukrainian",
+    menu: ['Вакансії', 'Для Бізнесу', 'Відправити Резюме']
   },
   {
     code: "en",
     pic: require('../assets/svg/flags/4x3/gb.svg').default,
-    name: "English"
+    name: "English",
+    menu: ['Vacancies', 'For Business', 'Submit Resume']
   },
   {
     code: "fr",
     pic: require('../assets/svg/flags/4x3/fr.svg').default,
-    name: "French"
+    name: "French",
+    menu: ['Offres d\'emploi', 'Pour les entreprises', 'Soumettre un CV']
   },
   {
     code: "de",
     pic: require('../assets/svg/flags/4x3/de.svg').default,
-    name: "German"
+    name: "German",
+    menu: ['Stellenangebote', 'Für Unternehmen', 'Lebenslauf einreichen']
   },
   {
     code: "it",
     pic: require('../assets/svg/flags/4x3/it.svg').default,
-    name: "Italian"
+    name: "Italian",
+    menu: ['Offerte di lavoro', 'Per le aziende', 'Invia il curriculum']
   },
   {
     code: "es",
     pic: require('../assets/svg/flags/4x3/es.svg').default,
-    name: "Spanish"
+    name: "Spanish",
+    menu: ['Ofertas de empleo', 'Para Empresas', 'Enviar Currículum']
   },
   {
     code: "tr",
     pic: require('../assets/svg/flags/4x3/tr.svg').default,
-    name: "Turkish"
+    name: "Turkish",
+    menu: ['İş İlanları', 'İşverenler için', 'Özgeçmiş Gönder']
   },
   {
     code: "az",
     pic: require('../assets/svg/flags/4x3/az.svg').default,
-    name: "Azerbaijani"
+    name: "Azerbaijani",
+    menu: ['Vakansiyalar', 'Biznes üçün', 'Özgeçmiş göndər']
   },
   {
     code: "bg",
     pic: require('../assets/svg/flags/4x3/bg.svg').default,
-    name: "Bulgarian"
+    name: "Bulgarian",
+    menu: ['Обяви за работа', 'За бизнеса', 'Изпрати резюме']
   },
   {
     code: "hr",
     pic: require('../assets/svg/flags/4x3/hr.svg').default,
-    name: "Croatian"
+    name: "Croatian",
+    menu: ['Poslovi', 'Za poslodavce', 'Pošaljite životopis']
   },
   {
     code: "fi",
     pic: require('../assets/svg/flags/4x3/fi.svg').default,
-    name: "Finnish"
+    name: "Finnish",
+    menu: ['Työpaikat', 'Yrityksille', 'Lähetä ansioluettelo']
   },
   {
     code: "hu",
     pic: require('../assets/svg/flags/4x3/hu.svg').default,
-    name: "Hungarian"
+    name: "Hungarian",
+    menu: ['Állásajánlatok', 'Vállalatoknak', 'Önéletrajz beküldése']
   },
   {
     code: "is",
     pic: require('../assets/svg/flags/4x3/is.svg').default,
-    name: "Icelandic"
+    name: "Icelandic",
+    menu: ['Staða', 'Fyrir Fyrirtæki', 'Senda ferilskrá']
   },
   {
     code: "lv",
     pic: require('../assets/svg/flags/4x3/lv.svg').default,
-    name: "Latvian"
+    name: "Latvian",
+    menu: ['Vakances', 'Uzņēmumiem', 'Iesūtīt dzīves aprakstu']
   },
   {
     code: "lt",
     pic: require('../assets/svg/flags/4x3/lt.svg').default,
-    name: "Lithuanian"
+    name: "Lithuanian",
+    menu: ['Darbo skelbimai', 'Įmonėms', 'Pateikti CV']
   },
   {
     code: "mt",
     pic: require('../assets/svg/flags/4x3/mt.svg').default,
-    name: "Maltese"
+    name: "Maltese",
+    menu: ['Vaganzi', 'Għal In-Negozju', 'Ibgħat ir-Riżumé']
   },
   {
     code: "no",
     pic: require('../assets/svg/flags/4x3/no.svg').default,
-    name: "Norwegian"
+    name: "Norwegian",
+    menu: ['Ledige stillinger', 'For Bedrifter', 'Send CV']
   },
   {
     code: "ro",
     pic: require('../assets/svg/flags/4x3/ro.svg').default,
-    name: "Romanian"
+    name: "Romanian",
+    menu: ['Locuri de Muncă', 'Pentru Companii', 'Trimite CV-ul']
   }
 ]
+
 export default lang
