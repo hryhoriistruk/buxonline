0382f1ad52dc9095e23f705d440045e4ca611a5f
emagweb <emagcodeshake@gmail.com>
2023-08-19 11:58:05 +0200

    Add Categories Title

---
+++ src/components/elements/Title.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react'
+import {useParams} from 'react-router-dom'
+import axios from 'axios'
+
+const Title = () => {
+
+  const { language } = useParams()
+  const [apiData, setApiData] = useState('Категорії')
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`https://api-dev.buxonline.org/api/v1/vacancy/list/?lang=${language}`)
+        setApiData(response.data)
+      } catch (error) {
+        console.error('Error fetching data:', error)
+      }
+    }
+
+    fetchData()
+  }, [language])
+
+  return (
+    <>
+      <div className="s-40"></div>
+      <h3 className="title-part mx-2">
+        {apiData.results[0] && apiData.results[0].meta && apiData.results[0].meta.categories}
+      </h3>
+      <div className="s-20"></div>
+    </>
+  )
+}
+
+export {Title}
